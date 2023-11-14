a83711b9682d2f0173708178d90bc9b9bbefb461
Adrien Didon <adrien.didon@telecomnancy.eu>
2023-11-14 15:30:53 +0100

    avancée lexeur

--- source/lexer.py
+++ source/lexer.py
@@ -1,44 +1,52 @@
 from TypeToken import mots_cles, typeToken
 from token import mots_cles, Token 
+import re
+
+identificateur_pattern = re.compile(r'^[a-zA-Z_]\w*$')
+
 
 def lexer(source_code):
     tokens = []
+    pastoken = []
     position = 0
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     chiffre = "0123456789"
     ident = alpha +chiffre+"_"
     while position < len(source_code):
         avancement = position
-        lecture = " "
+        lecture = ""
         #On teste d'abord si token
         for keyword in mots_cles:
             if source_code.startswith(keyword, position) and source_code[position + len(keyword)] not in ident:
                 tokens.append(Token(mots_cles[keyword], keyword, position))
+                pastoken.append()
                 position += len(keyword)
                 break
-        #Détection d'un nombre
-            elif source_code[position] in chiffre:
-                while source_code[avancement] in chiffre or source_code[avancement] == '.':
-                    lecture = lecture + source_code[avancement]
-                    avancement += 1
+        #C'est peut-être un identificateur
+        match = identificateur_pattern.match(source_code, position)
+        if match:
+            identificateur = match.group()
+            tokens.append(typeToken.IDENTIFICATEUR)
+            pastoken.append(identificateur)
+            position = match.end()
+        #Détection d'un nombre (proprement)
+        elif source_code[position] in chiffre:
+            while source_code[avancement] in chiffre :
+                lecture = lecture + source_code[avancement]
+                avancement += 1
+            tokens.append(typeToken.ENTIER,typeToken.POINT,typeToken.ENTIER)
+            pastoken.append(lecture)
+            position += len(lecture)
         #Détection d'une chaîne de caractères
-            elif source_code[position] == '"':
-                while source_code[avancement] in alpha or source_code[avancement] == " ":
-                    lecture = lecture + source_code[avancement]
-                    avancement += 1
-        #Détection d'une variable
-            elif source_code[position] in alpha:
-                while source_code[avancement] in alpha :
-                    lecture = lecture + source_code[avancement]
-                    avancement += 1
+        elif source_code[position] == '"' or source_code[position] == "'":
+            while source_code[avancement] != source_code[position]:
+                lecture = lecture + source_code[avancement]
+                avancement += 1
+            tokens.append(typeToken.CARACTERE)
+            pastoken.append(lecture)
+            position += len(lecture)
         #Détection d'un commentaire
-            elif source_code[position:position+1] == "--":
-                while source_code[position] != "\n":
-                    position += 1
-        
-
-        
-                
-
-
-    return tokens
+        elif source_code[position:position+1] == "--":
+            while source_code[position] != "\n":
+                position += 1
+    return tokens,pastoken
